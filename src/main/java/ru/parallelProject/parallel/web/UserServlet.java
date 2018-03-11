96048b89e62e2a300eebadc6837706241b640bb6
Manik Andrey <andre.ivan.manik@gmail.com>
2018-03-11 13:02:26 +0300

    changed add logging pom dependences and create logback.xml configurator

--- src/main/java/ru/parallelProject/parallel/web/UserServlet.java
+++ src/main/java/ru/parallelProject/parallel/web/UserServlet.java
@@ -1,10 +1,17 @@
 package ru.parallelProject.parallel.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 public class UserServlet extends javax.servlet.http.HttpServlet {
 
+    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+        LOG.debug("Request Dispatcher   ");
         request.getRequestDispatcher("/userList.jsp").forward(request, response);
         //response.sendRedirect("userList.jsp");
     }
