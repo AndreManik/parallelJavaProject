1e65d47e3fdfbfce9018ddcc545490dc2ac591ef
Manik Andrey <andre.ivan.manik@gmail.com>
2018-03-19 09:39:45 +0300

    HW03_added_JUNIT++

---
+++ src/test/java/ru/parallelProject/parallel/service/UserServiceImplTest.java
@@ -0,0 +1,32 @@
+package ru.parallelProject.parallel.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceImplTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void getByEmail() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
